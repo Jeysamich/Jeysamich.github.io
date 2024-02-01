bd17122d9d1da8746c156f4d880858aee4e3fb31
Jeysamich <156486935+Jeysamich@users.noreply.github.com>
2024-02-01 00:11:39 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,31 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    var passwordRegex =
+      /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\-])(?=.*[A-Z])(?=.*\d.*\d).{8,}$/;
+
+    var isValidEmail = emailRegex.test(email);
+    var isValidPassword = passwordRegex.test(password);
+
+    if (isValidEmail && isValidPassword) {
+      alert("Login exitoso");
+    } else {
+      event.preventDefault();
+      var errorMessage = "";
+      console.log("Email: " + email, "Password: " + password);
+      if (!isValidEmail) {
+        errorMessage += "El formato del correo electrónico no es válido. ";
+      }
+
+      if (!isValidPassword) {
+        errorMessage += "La contraseña no cumple con los requisitos mínimos.";
+      }
+
+      alert("Credenciales incorrectas. Inténtelo de nuevo.\n" + errorMessage);
+    }
+  });
