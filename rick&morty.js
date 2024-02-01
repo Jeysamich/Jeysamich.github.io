f5e011fa088e46a1dd37616a6da093674a9d5c3d
Jeysamich <156486935+Jeysamich@users.noreply.github.com>
2024-02-01 00:09:23 -0600

    Create rick&morty.js

---
+++ rick&morty.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const characterList = document.getElementById("character-list");
+  const characterTemplate = document.getElementById("character-template");
+  let lastCharacterId = 0;
+
+  async function fetchCharacterById(id) {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/${id}`
+    );
+    return response.json();
+  }
+
+  document.getElementById("spawn").addEventListener("click", async () => {
+    lastCharacterId++;
+
+    try {
+      const character = await fetchCharacterById(lastCharacterId);
+      const clone = characterTemplate.content.cloneNode(true);
+      clone.querySelector("[data-id='character-name']").textContent =
+        character.name;
+      clone.querySelector("[data-id='character-image']").src = character.image;
+      const listItem = document.createElement("li");
+      listItem.appendChild(clone);
+      characterList.appendChild(listItem);
+
+      VanillaTilt.init(listItem.querySelectorAll('[data-tilt]'), {
+        glare: true,
+        "max-glare": 0.5,
+      });
+
+      VanillaTilt.init(listItem.querySelectorAll('[data-tilt-glare]'), {
+        glare: {
+          maxOpacity: 0.5,
+        },
+      });
+    } catch (error) {
+      alert("No hay más personajes disponibles.");
+      lastCharacterId--;
+    }
+  });
+  
+
+  document.getElementById("clear").addEventListener("click", () => {
+    var itemList = document.getElementById("character-list");
+    itemList.replaceChildren();
+    lastCharacterId = 0;
+  });
+});
